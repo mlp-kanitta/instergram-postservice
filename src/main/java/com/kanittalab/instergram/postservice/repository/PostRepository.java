5abca31aea8cf390ab3b45d1381e1eb145f6a8f7
Kanitta Moonlapong <kanitta.moonlapong@scb.co.th>
2022-02-26 17:59:23 +0700

    Add delete,patch, getByUserId to PostRepository and PostService

--- src/main/java/com/kanittalab/instergram/postservice/repository/PostRepository.java
+++ src/main/java/com/kanittalab/instergram/postservice/repository/PostRepository.java
@@ -2,7 +2,16 @@ package com.kanittalab.instergram.postservice.repository;
 
 import com.kanittalab.instergram.postservice.model.Post;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.cassandra.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface PostRepository extends CassandraRepository<Post, String> {}
+public interface PostRepository extends CassandraRepository<Post, String> {
+    @Query("select * from post where user_id = ?0")
+    List<Post> findPostByUserId(String userId);
+
+    @Query("delete from post where id=?0 AND user_id = ?1")
+    int deletePostbyId(String id,String userId);
+}
--- src/main/java/com/kanittalab/instergram/postservice/service/PostService.java
+++ src/main/java/com/kanittalab/instergram/postservice/service/PostService.java
@@ -1,22 +1,25 @@
 package com.kanittalab.instergram.postservice.service;
 
+import com.kanittalab.instergram.postservice.constant.CommonConstants;
+import com.kanittalab.instergram.postservice.exception.BusinessException;
 import com.kanittalab.instergram.postservice.model.Post;
 import com.kanittalab.instergram.postservice.model.request.PostRequest;
 import com.kanittalab.instergram.postservice.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.cassandra.core.CassandraTemplate;
-import org.springframework.data.cassandra.core.query.CassandraPageRequest;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.time.Instant;
 import java.util.Base64;
 import java.util.List;
+import java.util.Optional;
+
 @Service("postService")
 public class PostService {
 
-   // @Autowired
-    private PostRepository feedRepository;
+    @Autowired
+    private PostRepository postRepository;
 
     @Autowired
     private CassandraTemplate cassandraTemplate;
@@ -27,19 +30,43 @@ public class PostService {
                 .queryForObject("select release_version from system.local", String.class);
     }
 
-    public Post createPost(PostRequest postRequest) throws IOException {
+    public Post createPost(PostRequest postRequest,String userId) throws IOException {
         Post post = new Post();
         post.setCaption(postRequest.getCaption());
         post.setContentType(postRequest.getImageFile().getContentType());
         post.setImage( Base64.getEncoder().encodeToString(postRequest.getImageFile().getBytes()));
+        post.setUserId(userId);
 
-        return feedRepository.insert(post);
+        return postRepository.insert(post);
     }
 
     public void deletePost(String id, String userId) {
+        postRepository.deletePostbyId(id,userId);
     }
 
     public List<Post> getPostsByUserId(String userId) {
-        return   feedRepository.findAll(CassandraPageRequest.first(10)).toList();
+        return postRepository.findPostByUserId(userId);
     }
+
+    public Post updatePost(String postId, String caption, String userId) throws BusinessException {
+        Post post = getPostById(postId);
+
+        if(null == post){
+            throw new BusinessException(CommonConstants.STATUS_CODE.STATUS_CODE_RECORD_NOT_FOUND,"Post not found");
+        }
+
+        post.setCaption(caption);
+        post.setUpdatedDate(Instant.now());
+        return postRepository.save(post);
+    }
+
+
+    public Post getPostById(String id) {
+        Optional<Post> postOptional = postRepository.findById(id);
+        if(postOptional!=null) {
+            return postOptional.get();
+        }
+        return null;
+    }
+
 }
