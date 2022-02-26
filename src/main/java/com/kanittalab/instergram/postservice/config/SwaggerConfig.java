e0d6575b7594a2a81a9bc5b801bed579f1fbb428
Kanitta Moonlapong <kanitta.moonlapong@scb.co.th>
2022-02-26 15:12:42 +0700

    Add SwaggerConfig

---
+++ src/main/java/com/kanittalab/instergram/postservice/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.kanittalab.instergram.postservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+import java.util.function.Predicate;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket produceApi(){
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("com.kanittalab.instergram.postservice.controller"))
+                .paths(PathSelectors.any())
+                .paths(Predicate.not(PathSelectors.regex("/error.*")))
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Instergram API")
+                .description("Instergram's PostService API ")
+                .build();
+    }
+
+
+}
