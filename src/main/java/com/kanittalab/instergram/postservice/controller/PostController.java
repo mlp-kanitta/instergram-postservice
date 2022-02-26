663c02fe15b330390feb359257ad300163eae805
Kanitta Moonlapong <kanitta.moonlapong@scb.co.th>
2022-02-26 17:58:30 +0700

    Fixed issue on POST /posts

--- src/main/java/com/kanittalab/instergram/postservice/controller/PostController.java
+++ src/main/java/com/kanittalab/instergram/postservice/controller/PostController.java
@@ -8,6 +8,7 @@ import com.kanittalab.instergram.postservice.model.Post;
 import com.kanittalab.instergram.postservice.model.request.PostRequest;
 import com.kanittalab.instergram.postservice.model.response.PostListResponse;
 import com.kanittalab.instergram.postservice.service.PostService;
+import com.kanittalab.instergram.postservice.utils.FileUtils;
 import jdk.jfr.ContentType;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
@@ -26,8 +27,13 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.IOException;
 import java.net.URI;
+import java.sql.Timestamp;
+import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.List;
 
 @Slf4j
@@ -48,17 +54,17 @@ public class PostController {
     @RequestMapping(path = "/posts", method = RequestMethod.POST, consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
     public ResponseEntity<CommonResponse> createPost(@RequestHeader("userid") String userId,
                                          @RequestPart  @NonNull MultipartFile imageFile,
-                                       @RequestParam @Max(250)  String caption) throws BusinessException, IOException {
+                                       @RequestParam @Size(max = 250) String caption) throws BusinessException, IOException {
 
         log.info("Received a posting request for userId : {}", userId);
 
-        if(imageFile.isEmpty()){
-            throw  new FileNotFoundException("Post image not found");
+        if(!FileUtils.isImageFile(imageFile)){
+            throw  new BusinessException(CommonConstants.STATUS_CODE.STATUS_CODE_INVALID_FILE_TYPE,"Post image not found");
         }
 
         PostRequest postRequest = new PostRequest(caption,imageFile);
 
-        Post post = postService.createPost(postRequest);
+        Post post = postService.createPost(postRequest,userId);
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentContextPath().path("/posts/{id}")
@@ -75,9 +81,11 @@ public class PostController {
      */
     @DeleteMapping("/posts/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletePost(@RequestHeader("userid") String userId, @PathVariable("id") String id) {
+    public ResponseEntity<CommonResponse> deletePost(@RequestHeader("userid") String userId, @PathVariable("id") String id) {
         log.info("Received request to delete a post id {} posted by userId {}", id, userId);
         postService.deletePost(id, userId);
+        return ResponseEntity.ok()
+                .body(new CommonResponse(CommonConstants.STATUS_CODE.STATUS_CODE_SUCCESS, "Deleted post successfully"));
     }
 
 
@@ -101,15 +109,16 @@ public class PostController {
      */
     @PatchMapping("/posts/{id}")
     public ResponseEntity<CommonResponse> updatePost(@RequestHeader("userid") String userId,
-                                        @RequestBody PostRequest postRequest) throws IOException {
+                                        @PathVariable("id") @NotNull String postId,
+                                                     @RequestParam @Size(max = 250) String caption) throws IOException, BusinessException {
 
         log.info("Received an updating request for userId : {}", userId);
 
-        Post post = postService.createPost(postRequest);
+        postService.updatePost(postId, caption, userId);
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentContextPath().path("/posts/{id}")
-                .buildAndExpand(post.getUid()).toUri();
+                .buildAndExpand(postId).toUri();
 
         return ResponseEntity
                 .created(location)
