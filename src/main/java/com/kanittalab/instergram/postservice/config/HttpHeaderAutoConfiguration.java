291937b2cc7f6a21dd82918c59fc7bb5f37e58aa
Kanitta Moonlapong <kanitta.moonlapong@scb.co.th>
2022-02-26 15:10:32 +0700

    Add HttpHeaderAutoConfiguration to autowired header as request scope

---
+++ src/main/java/com/kanittalab/instergram/postservice/config/HttpHeaderAutoConfiguration.java
@@ -0,0 +1,39 @@
+package com.kanittalab.instergram.postservice.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+@Slf4j
+@Configuration
+public class HttpHeaderAutoConfiguration {
+    @Bean
+    @RequestScope
+    public HttpHeaders httpHeaders() {
+        return httpHeadersSetup();
+    }
+
+    public static HttpHeaders httpHeadersSetup(){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        HttpServletRequest curRequest = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        Enumeration<String> headerNames = curRequest.getHeaderNames();
+        if (headerNames != null) {
+            while (headerNames.hasMoreElements()) {
+                String header = headerNames.nextElement();
+                String value = curRequest.getHeader(header);
+                log.info("Adding header {} with value {}", header, value);
+                    httpHeaders.add(header, value);
+            }
+        }
+        return httpHeaders;
+    }
+}
